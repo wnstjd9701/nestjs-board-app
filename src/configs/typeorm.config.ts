59957b2b8af094e704a5b9fe8e8fdd3ba2f3eb16
Yun Jun Sung <wnstjd9701@naver.com>
2023-02-04 19:01:21 +0900

    Modified: EntityRepository

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,12 +4,12 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: dbConfig.type,
-  host: process.env.RDS_HOSTNAME || dbConfig.host,
-  port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DB_NAME || dbConfig.database,
+  type: 'postgres',
+  host: 'host',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'board-app',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
 };
